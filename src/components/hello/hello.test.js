b55673ffad3477915d61d0e229e42a1eb904f697
nbstsh <nbstsh@gmail.com>
2019-08-12 20:24:28 +0900

    Snapshot testing

--- src/components/hello/hello.test.js
+++ src/components/hello/hello.test.js
@@ -1,37 +1,48 @@
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-import Hello from './hello.component';
+import pretty from "pretty";
+
+import Hello from "./hello.component";
 
 let container = null;
 
 beforeEach(() => {
-	// setup Dom element as a render target
-	container = document.createElement('div');
-	document.body.appendChild(container);
+  // setup Dom element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
 
 afterEach(() => {
-	// clean up on exiting
-	unmountComponentAtNode(container);
-	container.remove();
-	container = null;
+  // clean up on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
-it('renders with or without or without a name', () => {
-	act(() => {
-		render(<Hello />, container);
-	});
-	expect(container.textContent).toBe('Hey, stranger');
+it("renders with or without or without a name", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("Hey, stranger");
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
+    `"<span>Hey, stranger</span>"`
+  );
 
-	act(() => {
-		render(<Hello name="Jenny" />, container);
-	});
-	expect(container.textContent).toBe('Hello, Jenny!');
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Jenny!");
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
+    `"<h1>Hello, Jenny!</h1>"`
+  );
 
-	act(() => {
-		render(<Hello name="Mike" />, container);
-	});
-	expect(container.textContent).toBe('Hello, Mike!');
+  act(() => {
+    render(<Hello name="Mike" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Mike!");
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
+    `"<h1>Hello, Mike!</h1>"`
+  );
 });
