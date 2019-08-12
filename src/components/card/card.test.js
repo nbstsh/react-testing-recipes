da438c1b54721943d4d3dd7fa22c0ece51ac6bd6
nbstsh <nbstsh@gmail.com>
2019-08-12 20:08:12 +0900

    Timers

---
+++ src/components/card/card.test.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+import Card from './card.component';
+
+jest.useFakeTimers();
+
+let container = null;
+
+beforeEach(() => {
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+it('should select null after timing out', () => {
+	const onSelect = jest.fn();
+
+	act(() => {
+		render(<Card onSelect={onSelect} />, container);
+	});
+
+	act(() => {
+		jest.advanceTimersByTime(100);
+	});
+	expect(onSelect).not.toHaveBeenCalled();
+
+	act(() => {
+		jest.advanceTimersByTime(5000);
+	});
+	expect(onSelect).toHaveBeenCalledWith(null);
+});
+
+it('should cleanup on being removed', () => {
+	const onSelect = jest.fn();
+
+	act(() => {
+		render(<Card onSelect />, container);
+	});
+
+	act(() => {
+		jest.advanceTimersByTime(100);
+	});
+	expect(onSelect).not.toHaveBeenCalled();
+
+	act(() => {
+		render(null, container);
+	});
+
+	act(() => {
+		jest.advanceTimersByTime(5000);
+	});
+	expect(onSelect).not.toHaveBeenCalled();
+});
+
+it('should accept selections', () => {
+	const onSelect = jest.fn();
+
+	act(() => {
+		render(<Card onSelect={onSelect} />, container);
+	});
+
+	act(() => {
+		container
+			.querySelector('[data-testid="2"]')
+			.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+	});
+
+	expect(onSelect).toHaveBeenCalledWith(2);
+});
